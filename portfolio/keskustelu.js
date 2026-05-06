0e5940a28c58f6ad9c5cde94e3eeef9a84ed3407
Kuba <jl4607@edu.turku.fi>
2026-05-06 14:10:40 +0300

    päivitin keskustelun sivusto ja lisäsin uusi js tiedosto

--- portfolio/keskustelu.js
+++ portfolio/keskustelu.js
@@ -1,1 +1,26 @@
 console.log("Keskustelun sivu avattu");
+
+function send(event) {
+    event.preventDefault();
+
+    let birthdayField = document.getElementById("pvm");
+    let emailField = document.getElementById("email");
+    let typeField = document.getElementById("tyyppi");
+    let usageField = document.getElementById("suostumus");
+    let bodyField = document.getElementById("kommentti");
+
+    let birthday = birthdayField.value;
+    let email = emailField.value;
+    let type = typeField.value;
+    let usage = usageField.checked;
+    let body = bodyField.value;
+
+    console.log("Syntymäpäivä:", birthday);
+    console.log("Sähköposti:", email);
+    console.log("Tyyppi:", type);
+    console.log("Hyväksytty:", usage);
+    console.log("Kommentti:", body);
+
+    let dialog = document.getElementById("dialogi");
+    dialog.close();
+}
